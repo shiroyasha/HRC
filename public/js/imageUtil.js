8d347f26b7cd53d9ba6b44f05e314f5c29e86709
shiroyasha <igisar@gmail.com>
2012-08-15 01:51:59 +0200

    improved downsampling

---
+++ public/js/imageUtil.js
@@ -0,0 +1,97 @@
+var DownSampler = function() {
+    public = {};
+
+    function visual(x, y, w, h ) {
+
+        console.log( X, Y );
+        var canvas = document.getElementById("area");
+        // Get the 2D canvas context.
+        var c = canvas.getContext('2d');
+        c.save();
+        c.lineWidth = 1;
+        c.strokeStyle= '#f00';
+        c.strokeRect( X + x, Y + y, w, h );
+        c.restore();
+        
+    }
+
+    public.crop = function(image, log) {
+        var ret = {};
+        min_x = image.width;
+        min_y = image.height;
+        max_x = 0;
+        max_y = 0;
+        
+        for( var i = 0; i < image.height; i++ ) {
+          for( var j = 0; j < image.width; j++ ) {
+                
+            var b = image.data[i*(image.width*4) + j * 4 + 3] === 0;
+            if( log ) console.log( i, j, b );    
+            if( !b ) {
+              if( min_x >= j ) min_x = j;
+              if( min_y >= i ) min_y = i;
+              if( max_x < j ) max_x = j;
+              if( max_y < i ) max_y = i;     
+            }
+
+          }
+       }
+       ret.x = min_x;
+       ret.y = min_y;
+       ret.width = max_x - min_x;
+       ret.height = max_y - min_y;
+   
+       return (ret.width <= 0 || ret.height <= 0 ) ? null : ret;
+    }
+
+    function findPixel( image, area ) {
+
+      for( var i = 0; i < area.height; i++ ) {
+        for( var j = 0; j < area.width; j++ ) {
+
+          if( image.data[ (i + area.y)* image.width *4 + (j + area.x)*4 + 3] !== 0 ) {
+            return true;
+          } 
+
+        }
+      }
+      return false;      
+    }
+
+    public.downSample = function(image, grid) {
+       var ret = new Array(grid.height);
+       for( var i = 0; i < ret.length; i++) {
+           ret[i] = new Array(grid.width);
+       }
+
+       var area = public.crop(image);
+       if( area === null ) return null;
+
+       var w = Math.floor( area.width / grid.width);
+       var h = Math.floor( area.height / grid.height);
+       console.log( area, w, h );
+
+       for( var i = 0; i < grid.height; i++ ) {
+           for( var j = 0; j < grid.width; j++) {
+
+               var arg = {};
+               arg.x = area.x + j * w ;
+               arg.y = area.y + i * h ;
+
+               arg.width = ( j < grid.width - 1 ) ? w : area.width - (j-1)*w ;
+               arg.height = ( i < grid.height - 1 ) ? h : area.height - (i-1)*h ;
+              
+               //console.log( arg );
+               ret[i][j] = findPixel( image, arg );
+               visual( arg.x, arg.y, arg.width, arg.height );
+           }
+       }
+       visual( area.x, area.y, area.width, area.height );
+
+       console.log( ret );
+
+       return ret;
+    }    
+           
+    return public;
+};
--- public/js/submit.js
+++ public/js/submit.js
@@ -1,3 +1,5 @@
+var X, Y;
+
 (function() {
  
     var canvas;
@@ -6,126 +8,92 @@
     var WIDTH = 80;
     var HEIGHT = 80;
 
-    var HORIZONTAL = 5;
-    var VERTICAL = 7;
-
-    function crop( data ) {
-        var min_x = WIDTH, min_y = HEIGHT, max_x = 0, max_y = 0;
-
-        for( var i = 0; i < HEIGHT; i++ ) {
-            for( var j = 0; j < WIDTH; j++ ) {
-                
-                var b = data[i*(WIDTH*4) + j * 4 + 3] == 0;
-                
-                if( !b ) {
-                    //if( p + h == 0 ) console.log( data[i*(WIDTH*4) + j * 4],  data[i*(WIDTH*4) + j * 4 + 1], data[i*(WIDTH*4) + j * 4 + 2] );
-                    if( min_x > j ) min_x = j;
-                    if( min_y > i ) min_y = i;
-                    if( max_x < j ) max_x = j;
-                    if( max_y < i ) max_y = i;     
-                }
-
-            }
-        }
-
-        return { x : min_x, y : min_y, width : max_x - min_x, height : max_y - min_y }
-    }
-
-    function findPixel( x, y, width, height, data ) {
-        
-        //if( p + h == 0 ) console.log(x,  x + width ,y,  y + height, " -> ", data );
-
-        for( var i = 0; i < width + 1; i++ ) {
-            for( var j = 0; j < height + 1; j++ ) {
-
-                //if(p + h == 0) console.log("incrop", i, j, data[ (i + y)*WIDTH*4 + (j + x)*4 + 3] == 0 );
-
-                var b = data[ (i + y)*WIDTH*4 + (j + x)*4 + 3] == 0;
-
-                if( !b ) return true;
-
-            }
-        }
-
-        return false;
-    }
-
-    function downSampleRegion( ii, jj, data ) {
-
-        var rez = [];
-        var size = crop( data );
-
-        var w = Math.floor( size.width / HORIZONTAL );
-        var h = Math.floor( size.height / VERTICAL );
-
-        if( ii + jj == 0 ) console.log( w, h );
-
-        for( var i = 0; i < VERTICAL; i++ ) {
-            for( var j = 0; j < HORIZONTAL; j++ ) {
-                var fx = size.x + j*w;
-                var fy = size.y + i*h;
-                var fw = w;
-                var fh = h;
-
-                if( j == HORIZONTAL - 1 ) fw = size.width - j * w;
-                if ( i == VERTICAL - 1 ) fh = size.height - i * h;
-                
-                //if( ii + jj == 0 ) console.log("crop", size.x + j * w , size.y + i * h, w, h, " -> ", f );
-                //if( j==HORIZONTAL || i == VERTICAL ) console.log('special case');
-                //console.log( fx, fy, fw, fh);
-                rez.push( findPixel(fx, fy, fw, fh, data) ? 1 : 0 );
-            }
-        }
-        /*
-        //if( ii + jj == 0 ) { 
-            console.log( ii, jj, " -> ", size.x, size.y, size.width, size.height );
-            //console.log( "rez", rez );
-            for( var i = 0; i < VERTICAL; i++ ) {
-
-                var row = "";
-                for( var j = 0; j < HORIZONTAL; j++ ) {
-                    row += rez[i*HORIZONTAL + j] ? '# ' : '_ ';
-                }
-
-                console.log(row);
-            }
-        //}
-        */
-
-        //console.log( rez );
-        return rez;
-    }
+    var PANE = { width : 12, height : 5 };
+    var GRID = { width : 10, height : 14 };
 
     function downSample() {
         var rez = [];
 
-        for( var i = 0; i < 12; i++ ) {
-           for( var j = 0; j < 5; j++ ) {
-                var d = context.getImageData( i * WIDTH, j * HEIGHT, WIDTH, HEIGHT );
+        var downSampler = DownSampler();
+
+        for( var j = 0; j < PANE.height ; j++ ) {
+           for( var i = 0; i < PANE.width ; i++ ) {
+                var image = context.getImageData( i * WIDTH, j * HEIGHT, WIDTH, HEIGHT );
+
+                X = i * WIDTH;
+                Y = j * HEIGHT;
                 
-                rez.push( downSampleRegion( i, j, d.data ) );
-           } 
+                rez.push( downSampler.downSample( image, GRID ) );
+           }
         }
 
-        console.log( rez.length );
-
-        //for( var i = 0; i < rez.length; i++)
-        //    console.log( rez[i] );
-        
         return rez;
     }
 
+    function renderDownsampled(rez) {
+        $('#answer').append("<canvas width=961 height=401 id='area2'> </canvas>");
+
+        var canvas2 = document.getElementById("area2");
+        var c2 = canvas2.getContext('2d');
+
+        for( var i = 0; i < rez.length; i++) {
+            
+            if( rez[i] === null ) continue;
+
+            for( var y = 0; y < GRID.height; y++) {
+
+                for( var x = 0; x < GRID.width; x++) {
+
+                    if( rez[i][y][x] === true ) {
+                        
+                        var w = 70 / GRID.width;
+                        var h = 70 / GRID.height;
+                        
+                        c2.fillRect( 5 + (i % 12) * 80 + x*w ,
+                                     5 +  Math.floor( i / 12 ) * 80 + y*h,
+                                      w,
+                                      h );
+                    }  
+                }
+            }
+        }
+
+    }
+
+    function renderData(rez) {
+        for( var i = 0; i < rez.length; i++) { 
+            if( rez[i] === null ) continue;
+
+            var d = "[ ";
+            for( var y = 0; y < GRID.height; y++ ) {
+                for( var x = 0; x < GRID.width; x++) {
+                    var b = rez[i][y][x] ? 1 : 0;
+                    d += ( x == GRID.width - 1 && y == GRID.height - 1) ? b : b + ", " ;    
+                }
+            }    
+            d += i === rez.lenght - 1 ? "]" : "],";
+            $("#answer").append("<p>" + d + "</p>");
+        }
+    }
+
     function submit() {
-        $('#answer').html('yay')
+        $("#answer").html(""); 
 
         var result = downSample();
 
+        renderDownsampled(result);
+        renderData(result); 
+        
+        /*
         for( var i = 0; i < result.length; i++ ) {
             $.post('/', { data : result[i]}, function(data) {
                 console.log(i, " -> ", data );
             });
         }
+        */
     }
+
+
 
     $( function() {
 
